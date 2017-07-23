3ca92cfe8ea908b3c7f0e8261f61bcb45f7fab24
Mark Van de Vyver <mark@taqtiqa.com>
2017-07-23 15:02:04 +1000

    ACI container build tool install script
    Working on ubuntu 16.04 desktop.
    Supporting Travis configuration not determined yet.
    
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ci
-Continuous integration scripts
+TAQTIQA LLC continuous integration scripts
+
+## Travis
+**IMPORTANT:**
+  - The .travis.yml must pipe scripts to bash (not to sh)!  In the Travis CI environment a #!/bin/bash shebang here won't help.
+
+### Install Container Build Tool: `acbuild`
+**Assumptions:**
+  - Git > 1.9.0.  To deepen the shallow clone `git fetch --unshallow`.
+
+Travis config:
+````yaml
+before_install:
+  - git clone https://github.com/taqtiqa/ci.git
+  - cat ci/travis/install-acbuild.sh | sudo bash # pipe to bash not sh!
+````
+
+See [acbuild docs](https://github.com/containers/build/blob/master/Documentation/getting-started.md)
---
+++ travis/install-acbuild.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2017 TAQTIQA LLC. <http://www.taqtiqa.com>
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License v3
+#along with this program.
+#If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+#
+
+# IMPORTANT: Your .travis.yml must pipe this script to bash (not to sh)!
+# In the Travis CI environment a #!/bin/bash shebang here won't help.
+
+set -eoux pipefail
+
+if [ "$EUID" -ne 0 ]; then
+    echo "This script uses functionality which requires root privileges"
+    exit 1
+fi
+
+function json_value() {
+  KEY=${1-'key'}
+  POS=${2-''}
+  awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed 's/\\n/\n/g' | sed -n ${POS}p
+}
+
+BUILD_SLUG='containers/build' # Idiosyncratic naming prevents using $BUILD_NAME
+BUILD_NAME='acbuild'
+BUILD_BIN_DIR="/opt/${BUILD_NAME}/bin"
+BUILD_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/${BUILD_SLUG}/releases/latest | json_value tag_name)
+
+NAME=${BUILD_NAME:-$DEFAULT_BUILD_NAME}
+SLUG=${BUILD_SLUG:-$DEFAULT_BUILD_SLUG}
+VERSION=${BUILD_VERSION:-$DEFAULT_BUILD_VERSION}
+BIN_DIR=${BUILD_BIN_DIR:-$DEFAULT_BUILD_BIN_DIR}
+
+ARTIFACT="${NAME}-${VERSION}.tar.gz"
+ARTIFACT_URL="https://github.com/${SLUG}/releases/download/${VERSION}/${ARTIFACT}"
+
+pushd /tmp
+  #git clone https://github.com/${SLUG}.git ./${NAME}
+  curl -L -o "${ARTIFACT}" "${ARTIFACT_URL}"
+  curl -L -o "${ARTIFACT}.asc" "${ARTIFACT_URL}.asc"
+  mkdir -p ./${NAME}
+  tar zxvf $ARTIFACT -C ./${NAME} --strip-components=1
+  pushd ./${NAME}
+    ls -la
+    #./build
+    sudo mkdir -p /opt/${NAME}/bin
+    sudo yes | cp -rf * $BIN_DIR
+    export PATH=$PATH:$BIN_DIR
+  popd
+popd
+echo "Success"
