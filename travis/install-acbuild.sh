cca6b7ec8aa8bbd2cca83c23af5259a2c932c8ba
Mark Van de Vyver <mark@taqtiqa.com>
2017-07-25 19:40:58 +1000

    Add container build required system packages.
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- travis/install-acbuild.sh
+++ travis/install-acbuild.sh
@@ -69,7 +69,7 @@ esac
 
 case $DISTRIB_CODENAME in
      trusty)
-          apt-get -y install golang-go debootstrap-udeb systemd
+          apt-get -y install golang-go debootstrap systemd
           ;;
      xenial)
           apt-get -y install golang-go bootstrap-base systemd-container
