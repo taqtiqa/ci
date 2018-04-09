31d0b0abc500dc4e74ca6e23a4cd8ada3365b000
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-10 05:46:03 +1000

    Skopeo and docker2aci

--- scripts/install-skopeo.sh
+++ scripts/install-skopeo.sh
@@ -2,6 +2,7 @@
 # sudo apt-add-repository ppa:projectatomic/ppa 
 # sudo apt-get update
 # sudo apt-get install golang 
+go get github.com/containers/image
 git clone https://github.com/projectatomic/skopeo $GOPATH/src/github.com/projectatomic/skopeo
 cd $GOPATH/src/github.com/projectatomic/skopeo 
 make binary-local
