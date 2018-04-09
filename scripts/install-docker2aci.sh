23469e96d4c859cd437d2a1f1ac39d12ea4a924a
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-09 22:09:50 +1000

    Skopeo and docker2aci

--- scripts/install-docker2aci.sh
+++ scripts/install-docker2aci.sh
@@ -3,3 +3,4 @@ git clone git://github.com/appc/docker2aci
 cd docker2aci
 sudo ./build.sh
 cp ./bin/docker2aci ${HOME}/.local/bin/docker2aci
+sudo chmod +x ${HOME}/.local/bin/docker2aci
--- scripts/install-skopeo.sh
+++ scripts/install-skopeo.sh
@@ -1,4 +1,7 @@
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:projectatomic/ppa 
 sudo apt-get update
-sudo apt-get install skopeo 
+# sudo apt-get install skopeo 
+git clone https://github.com/projectatomic/skopeo $GOPATH/src/github.com/projectatomic/skopeo
+cd $GOPATH/src/github.com/projectatomic/skopeo 
+make binary-local
