dca4be3559336793876528a31a33910791463f00
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-12 06:32:27 +1000

    Skopeo and docker2aci

--- scripts/install-skopeo.sh
+++ scripts/install-skopeo.sh
@@ -1,14 +1,6 @@
 # sudo apt-get install software-properties-common
 # sudo apt-add-repository ppa:projectatomic/ppa 
 
-cat << EOF >> /etc/apt/sources.list
-deb http://archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse  
-deb http://archive.ubuntu.com/ubuntu/ trusty-updates main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse
-deb http://security.ubuntu.com/ubuntu trusty-security main restricted universe multiverse  
-deb http://archive.canonical.com/ubuntu trusty partner    
-deb http://extras.ubuntu.com/ubuntu trusty main   
-EOF
 apt-key adv --keyserver p80.pool.sks-keyservers.net:80 --recv-keys 6A030B21BA07F4FB
 apt-key adv --keyserver p80.pool.sks-keyservers.net:80 --recv-keys 16126D3A3E5C1192
 apt-get update
@@ -17,5 +9,6 @@ apt-get install -qq libgtk2.0-dev libglib2.0-dev libgpgme11-dev libassuan-dev bt
 go get github.com/containers/image
 git clone https://github.com/projectatomic/skopeo $GOPATH/src/github.com/projectatomic/skopeo
 cd $GOPATH/src/github.com/projectatomic/skopeo 
-make binary-local
+
+make binary-local BUILDTAGS=containers_image_ostree_stub
 make install
