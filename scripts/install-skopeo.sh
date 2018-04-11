11b6c05f7a9b186f439b2e4b292a59fa1e1f9b0c
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-12 04:27:30 +1000

    Skopeo and docker2aci

--- scripts/install-skopeo.sh
+++ scripts/install-skopeo.sh
@@ -1,9 +1,17 @@
 # sudo apt-get install software-properties-common
 # sudo apt-add-repository ppa:projectatomic/ppa 
-# sudo apt-get update
+
+cat << EOF >> /etc/apt/sources.list
+deb http://archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse  
+deb http://archive.ubuntu.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu trusty-security main restricted universe multiverse  
+deb http://archive.canonical.com/ubuntu trusty partner    
+deb http://extras.ubuntu.com/ubuntu trusty main   
+EOF
+sudo apt-get update
 # sudo apt-get install golang 
 sudo apt-get install -qq libgpgme-dev libassuan-dev btrfs-tools libdevmapper-dev
- # ostree-devel
 go get github.com/containers/image
 git clone https://github.com/projectatomic/skopeo $GOPATH/src/github.com/projectatomic/skopeo
 cd $GOPATH/src/github.com/projectatomic/skopeo 
