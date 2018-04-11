d4cf55ddb31083d7d6a1b87c200b36a76b93122c
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-12 05:58:57 +1000

    Skopeo and docker2aci

--- scripts/install-skopeo.sh
+++ scripts/install-skopeo.sh
@@ -9,6 +9,8 @@ deb http://security.ubuntu.com/ubuntu trusty-security main restricted universe m
 deb http://archive.canonical.com/ubuntu trusty partner    
 deb http://extras.ubuntu.com/ubuntu trusty main   
 EOF
+apt-key adv --keyserver p80.pool.sks-keyservers.net:80 --recv-keys 6A030B21BA07F4FB
+apt-key adv --keyserver p80.pool.sks-keyservers.net:80 --recv-keys 16126D3A3E5C1192
 apt-get update
 # sudo apt-get install golang 
 apt-get install -qq libgpgme11-dev libassuan-dev btrfs-tools libdevmapper-dev
