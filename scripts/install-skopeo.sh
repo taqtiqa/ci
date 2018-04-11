4cf27346770b5df9e8b4b15f82e4f634f2f44e95
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-11 21:09:14 +1000

    Skopeo and docker2aci

--- scripts/install-skopeo.sh
+++ scripts/install-skopeo.sh
@@ -5,8 +5,8 @@
 sudo apt-get libgpgme11-dev libassuan-dev btrfs-tools libdevmapper-dev
  # ostree-devel
 export PATH=/root/gopath/bin:$PATH
-# go get github.com/containers/image
-# git clone https://github.com/projectatomic/skopeo $GOPATH/src/github.com/projectatomic/skopeo
-# cd $GOPATH/src/github.com/projectatomic/skopeo 
-# make binary-local
-# sudo make install
+go get github.com/containers/image
+git clone https://github.com/projectatomic/skopeo $GOPATH/src/github.com/projectatomic/skopeo
+cd $GOPATH/src/github.com/projectatomic/skopeo 
+make binary-local
+sudo make install
