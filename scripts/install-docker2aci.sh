9c52cbd4b72a0ad4ab2956eac8908826ffc88271
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-12 10:32:21 +1000

    Skopeo and docker2aci

--- scripts/install-docker2aci.sh
+++ scripts/install-docker2aci.sh
@@ -2,6 +2,5 @@
 git clone git://github.com/appc/docker2aci
 cd docker2aci
 ./build.sh
-mkdir -p ${HOME}/.local/bin
-cp ./bin/docker2aci ${HOME}/.local/bin/docker2aci
-chmod +x ${HOME}/.local/bin/docker2aci
+cp ./bin/docker2aci /usr/local/bin/docker2aci
+chmod +x /usr/local/bin/docker2aci
